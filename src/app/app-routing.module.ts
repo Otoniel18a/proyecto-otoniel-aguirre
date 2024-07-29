0c34ccb321e1348073b7fa924aba7c7e9c334142
Otoniel <otoniel18a@gmail.com>
2024-07-28 20:39:33 -0600

    Creacion de pagina cafta terminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
+import { CaftaComponent } from './pages/cafta/cafta.component';
+import { OrganizacionComponent } from './pages/organizacion/organizacion.component';
 
 const routes: Routes = [
   {path: 'index', component: InicioComponent},
   {path: 'nosotros', component: NosotrosComponent},
   {path: 'contacto', component: ContactoComponent},
+  {path: 'cafta', component: CaftaComponent},
+  {path: 'organizacion', component: OrganizacionComponent},
 
   {
     path: '**',
